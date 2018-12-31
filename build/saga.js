e81d14313ced1fc304fe64c01d29bfcdc48e536c
Akshay Nair <phenax5@gmail.com>
2018-12-31 21:38:13 +0530

    Adds helper to putResponse (composing response cata + ActionType)

--- build/saga.js
+++ build/saga.js
@@ -3,11 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.callAsync = void 0;
+exports.putResponse = exports.callAsync = void 0;
 
 var _effects = require("redux-saga/effects");
 
 var _async = require("./async");
+
+// id :: a -> a
+var id = function id(x) {
+  return x;
+}; // callAsync :: (() -> Async a) -> Saga.IO a
+
 
 var callAsync = function callAsync(fn) {
   for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
@@ -15,6 +21,32 @@ var callAsync = function callAsync(fn) {
   }
 
   return _effects.call.apply(void 0, [(0, _async.toPromiseResponse)(fn)].concat(args));
-};
+}; // putResponse :: (ActionType, Response a, { map, mapFailure :: a -> b }) -> Saga.IO b
+
 
 exports.callAsync = callAsync;
+
+var putResponse = function putResponse(subType, response) {
+  var _ref = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {},
+      _ref$map = _ref.map,
+      map = _ref$map === void 0 ? id : _ref$map,
+      _ref$mapFailure = _ref.mapFailure,
+      mapFailure = _ref$mapFailure === void 0 ? id : _ref$mapFailure;
+
+  return (0, _effects.put)(response.cata({
+    Success: function Success(data) {
+      return {
+        type: subType.SUCCESS,
+        payload: map(data)
+      };
+    },
+    Failure: function Failure(error) {
+      return {
+        type: subType.FAILURE,
+        payload: mapFailure(error)
+      };
+    }
+  }));
+};
+
+exports.putResponse = putResponse;
--- src/saga.js
+++ src/saga.js
@@ -1,4 +1,14 @@
-import { call } from 'redux-saga/effects';
+import { call, put } from 'redux-saga/effects';
 import { toPromiseResponse } from './async';
 
+// id :: a -> a
+const id = x => x;
+
+// callAsync :: (() -> Async a) -> Saga.IO a
 export const callAsync = (fn, ...args) => call(toPromiseResponse(fn), ...args);
+
+// putResponse :: (ActionType, Response a, { map, mapFailure :: a -> b }) -> Saga.IO b
+export const putResponse = (subType, response, { map = id, mapFailure = id } = {}) => put(response.cata({
+  Success: data => ({ type: subType.SUCCESS, payload: map(data) }),
+  Failure: error => ({ type: subType.FAILURE, payload: mapFailure(error) }),
+}));
